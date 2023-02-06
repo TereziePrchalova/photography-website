c849e3b497fc6cbb1842d03c4d8f595d6f01d83a
Terezie <terezie.prchalova1@gmail.com>
2023-02-06 16:52:28 +0100

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -118,7 +118,7 @@ export default function Navbar(data) {
             </div>
             
             <div className='absolute bottom-[245px] right-8'>
-              <svg width="1" height="250" viewBox="0 0 1 250" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="1" height="240" viewBox="0 0 1 240" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <line x1="0.5" y1="439.018" x2="0.5" y2="-3.05176e-05" stroke="#565656"/>
               </svg>
             </div>
