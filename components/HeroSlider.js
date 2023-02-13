7a06aa7f0cc11519922ec9130cc831cf86478f31
Terezie <terezie.prchalova1@gmail.com>
2023-02-13 13:31:21 +0100

    Progress

--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -32,11 +32,11 @@ export default function HeroSlider(data) {
             <div className='hidden px-6 my-4 md:block'>
                 <div className="flex">
                     <div className="relative w-[292px] h-[437px]">
-                        <Image src="/images/slider/heroSliderSecond.jpg" alt="me" objectFit="cover" layout="fill"/>
+                        <Image src="/images/slider/heroSliderSecond.jpeg" alt="me" objectFit="cover" layout="fill"/>
                     </div>
                     <div className="ml-4 flex flex-col justify-between">
                         <div className="relative w-[279px] h-[203px]">
-                            <Image src="/images/slider/heroSliderFirst.jpg" alt="me" objectFit="cover" layout="fill"/>
+                            <Image src="/images/slider/heroSliderFirst.jpeg" alt="me" objectFit="cover" layout="fill"/>
                         </div>
                         <h2 className="h2">Moments that matter</h2>
                         <p className="p w-[300px] mt-1 mb-6">If you're looking for an artist to capture your milestones and moments in-between, we just might be a match made in heaven!</p>
@@ -47,7 +47,7 @@ export default function HeroSlider(data) {
                 </div>
                 <div className="flex flex-col mt-8 mb-12">
                     <div className="relative w-[443px] h-[266px] mr-3">
-                        <Image src="/images/slider/heroSliderFirst.jpg" alt="me" objectFit="cover" layout="fill"/>
+                        <Image src="/images/slider/heroSliderFirst.jpeg" alt="me" objectFit="cover" layout="fill"/>
                     </div>
                     <div className="flex">
                         <div className="relative w-[125px] h-[125px] mb-3">
