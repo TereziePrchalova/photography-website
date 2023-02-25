91f419b5dc89d3085cc45602da9576321d058c22
Terezie <terezie.prchalova1@gmail.com>
2023-02-25 14:09:49 +0100

    Progress

--- components/HeroText.js
+++ components/HeroText.js
@@ -2,7 +2,7 @@ export default function HeroText(data) {
     return (
         <>
             <div className='max-w-[1440px] m-auto md:px-8 xl:px-12 xl:mb-12'>
-                <h1 className='h1 leading-10 w-[330px] m-auto md:leading-tight md:w-[696px] lg:w-auto xl:mb-2'>Wedding photographer</h1>
+                <h1 className='h1 leading-10 w-[330px] m-auto md:leading-tight md:w-[696px] lg:w-auto xl:mb-2'>Lifestyle photographer</h1>
                 <p className='copy w-[330px] m-auto md:w-[696px]'>You wrote the story, let me illustrate it!</p>
             </div>
         </>
