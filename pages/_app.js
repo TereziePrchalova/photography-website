ab3c0fe552b85a65ea5f4609c76ba0cdcbf246d0
Terezie <terezie.prchalova1@gmail.com>
2023-04-16 13:24:58 +0200

    Progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import '../styles/globals.css'
-import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Calendly from '../components/Calendly'
-import Gallery from '../components/Gallery'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,9 +10,6 @@ function MyApp({ Component, pageProps }) {
         <title>Terezie Prchalova</title>
       </Head>
       <div>
-        <div className='hidden'>
-          <Calendly/>
-        </div>
         <Component {...pageProps} />
         <Footer/>
       </div>
