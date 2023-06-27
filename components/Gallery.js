4d3692f30142f3a34aca3d97160b04a471bb98ae
Terezie <terezie.prchalova1@gmail.com>
2023-06-27 19:07:54 +0200

    Updates

--- components/Gallery.js
+++ components/Gallery.js
@@ -124,6 +124,10 @@ export default function Gallery({section}) {
             image: "/images/weddings/11.jpg",
             category: "wedding",
         },
+        {
+            image: "/images/weddings/15.jpg",
+            category: "wedding",
+        },
     ]
 
     const [items, setItems] = useState(images);
