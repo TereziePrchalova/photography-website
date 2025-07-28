3d4309ee8d7a740b50bb4733b3c7c2214821e0bd
Terezie <terezie.prchalova1@gmail.com>
2025-07-28 19:55:27 +0200

    Contact padding

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ export default function contact() {
       <div className="fixed w-full top-0 z-50">
         <Navbar/>
       </div>
-      <div className="max-w-1440px m-auto md:px-8 xl:px-12 xl:mb-6">
+      <div className="max-w-1440px m-auto px-4 md:px-8 xl:px-12 xl:mb-6">
         <h1 className='h1 w-330px m-auto md:w-696px lg:w-942px xl:w-1188px 2xl:w-auto'>Contact</h1>
           <h2 className="font-PoppinsMedium text-20px mt-8 mb-4">Terezie Prchalová</h2>
           <p className="">Email</p>
